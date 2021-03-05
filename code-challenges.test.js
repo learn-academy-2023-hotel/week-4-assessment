63b355da94af1606cf8dea42620bf98f5a8cf562
sjproctor <sjproctor950@gmail.com>
2021-03-05 11:20:28 -0800

    adds updates for alpha

---
+++ code-challenges.test.js
@@ -0,0 +1,105 @@
+// ASSESSMENT 4: JavaScript Coding Practical Questions with Jest
+
+// Please read all questions thoroughly
+// Pseudo coding is HIGHLY recommended
+// If you get stuck, please leave comments to help us understand your thought process
+
+// Add appropriate dependencies to the repository:
+// $ yarn add jest
+
+// Use test driven development to complete the following questions
+// Run the file with the following command:
+// $ yarn jest
+
+// Reminder: The test will call your function
+
+
+// --------------------1) Create a function that takes in an array, removes the first item from the array and shuffles the remaining content.
+
+// a) Create a test with an expect statement using the variable provided.
+
+var colors1 = ["purple", "blue", "green", "yellow", "pink"]
+// Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
+var colors2 = ["chartreuse", "indigo", "periwinkle", "ochre", "aquamarine", "saffron"]
+// Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
+
+
+
+
+
+// b) Create the function that makes the test pass.
+
+
+
+
+
+// --------------------2) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
+
+// a) Create a test with expect statements for each of the variables provided.
+
+var cubeAndSum1 = [2, 3, 4]
+// Expected output: 99
+var cubeAndSum2 = [0, 5, 10]
+// Expected output: 1125
+
+
+
+
+
+// b) Create the function that makes the test pass.
+
+
+
+
+
+// --------------------3) Create a function that takes an array of numbers and returns an array of the minimum and maximum numbers in that order.
+
+// a) Create a test with expect statements for each of the variables provided.
+
+var nums1 = [3, 56, 90, -8, 0, 23, 6]
+// Expected output: [-8, 90]
+var nums2 = [109, 5, 9, -59, 8, 24]
+// Expected output: [-59, 109]
+
+
+
+
+
+// b) Create the function that makes the test pass.
+
+
+
+
+
+// --------------------4) Create a function that takes in a string and returns a string with every other letter capitalized.
+
+// a) Create a test with expect statements for each of the variables provided.
+
+var testString1 = "albatross"
+// Expected output: "aLbAtRoSs"
+var testString2 = "jabberwocky"
+// Expected output: "jAbBeRwOcKy"
+
+
+
+
+
+// b) Create the function that makes the test pass.
+
+
+
+
+
+// --------------------5) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.
+
+// a) Create a test with an expect statement using the variables provided.
+
+var testArray1 = [3, 7, 10, 5, 4, 3, 3]
+var testArray2 = [7, 8, 2, 3, 1, 5, 4]
+// Expected output: [3, 7, 10, 5, 4, 8, 2, 1]
+
+
+
+
+
+// b) Create the function that makes the test pass.
--- code_challenges.rb
+++ code_challenges.rb
@@ -1,4 +1,5 @@
 # ASSESSMENT 4: Ruby Coding Practical Questions
+# MINASWAN ✌️
 
 # --------------------1) Create a method that takes in an array and returns an array with only odd numbers sorted from least to greatest.
 
@@ -11,38 +12,40 @@ fullArr2 = ['hello', 7, 23, -823, false, 78, nil, '67', 6, 'Number']
 
 
 
-# --------------------2) Create a class called Bike that is initialized with a model, wheels, and a frame size. The default number of wheels is 2. Create a get_info method that returns a sentence with all the data from the bike object.
+# --------------------2) Create a method that takes in an array of words and a letter and returns all the words that contain that particular letter.
+
+beverages_array = ['coffee', 'tea', 'juice', 'water', 'soda water']
+letter_o = 'o'
+# Expected output: ['coffee', 'soda water']
+letter_a = 'a'
+# Expected output: ['tea', 'soda water']
 
 
 
 
 
-# Expected output example: 'The Trek bike has 2 wheels and a 168cm frame.'
+# -------------------3) Create a method that takes in a string and removes all the vowels from the string. Use the test variables provided. HINT: Check out this resource: https://ruby-doc.org/core-2.6/String.html#method-i-delete
 
-
-# -------------------3) Add a bell to the bike class and create a method that will ring the bell when the method is called.
-
-
-
-
-
-# Expected output example: my_bike.ring_bell => 'Beep beep!'
-
-
-# -------------------4) Add a speedometer to the Bike class. The speed should be initialized at 0.
+album1 = 'Rubber Soul'
+# Expected output: 'Rbbr Sl'
+album2 = 'Sgt Pepper'
+# Expected output: 'Sgt Pppr'
+album3 = 'Abbey Road'
+# Expected output: 'bby Rd'
 
 
 
 
 
-# Expected output example: my_bike.speed => 0
+# --------------------4) Create a class called Bike that is initialized with a model, wheels, and current_speed. The default number of wheels is 2. The current_speed should start at 0. Create a get_info method that returns a sentence with all the data from the bike object.
+
+# Expected output example: 'The Trek bike has 2 wheels and is going 0 mph.'
+
+
+
 
 
 # -------------------5) Add the ability to pedal faster and brake. The pedal_faster method should increase the speed. The brake method should decrease the speed. The bike cannot go negative speeds.
-
-
-
-
 
 # Expected output example: my_bike.pedal_faster 10 => 10
 # Expected output example: my_bike.brake 15 => 0
